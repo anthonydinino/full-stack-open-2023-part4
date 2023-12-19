eb123dac0cf0b0780e77aff3d97375c068ee862a
anthonydinino <adinino1996@gmail.com>
2023-12-20 01:21:19 +1030

    move file

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,7 +3,7 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
-const helper = require("./blog_helper");
+const helper = require("../utils/blog_helper");
 
 mongoose.set("bufferTimeoutMS", 30000);
 
