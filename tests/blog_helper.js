56111de8ed40e4010a1de0443692d6bee755e863
anthonydinino <adinino1996@gmail.com>
2023-12-20 01:19:10 +1030

    Exercise 4.12 complete

--- tests/blog_helper.js
+++ tests/blog_helper.js
@@ -56,5 +56,15 @@ const blogs = [
     __v: 0,
   },
 ];
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
 
-module.exports = { blogs };
+module.exports = { blogs, listWithOneBlog };
--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,22 +1,13 @@
 const listHelper = require("../utils/list_helper");
-const blogs = require("./blog_helper").blogs;
+const blogHelper = require("./blog_helper");
+const listWithOneBlog = blogHelper.listWithOneBlog;
+const blogs = blogHelper.blogs;
 
 test("dummy returns one", () => {
   const blogs = [];
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
-
-const listWithOneBlog = [
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0,
-  },
-];
 
 describe("total likes", () => {
   test("of empty list is zero", () => {
